19ab89872fd827888f77940aebb5ed0993cd6521
neko <appyoohoy@gmail.com>
2021-09-10 17:50:24 +0900

    テスト

---
+++ git_test.cpp
@@ -0,0 +1,9 @@
+#define _GLIBCXX_DEBUG
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define srep(i, s, n) for (int i = (s); i < (int)(n); i++)
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "Hello World" << endl;
+}
